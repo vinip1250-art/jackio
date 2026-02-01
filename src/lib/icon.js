31fc48334e5f9a5ea60e8295b9d4f6cc5d9507c6
vinip1250-art <vinip1250@gmail.com>
2026-02-01 06:09:24 -0300

    Refactor icon.js to simplify icon handling
    Removed download and getLocation functions, and added iconPath constant.

--- src/lib/icon.js
+++ src/lib/icon.js
@@ -1,34 +1,16 @@
-import { writeFile, readFile } from 'node:fs/promises';
-import path from 'path';
-import config from './config.js';
+import path from 'path'
 
-let ICON_LOCATION = path.join(import.meta.dirname, '../static/img/icon.png');
+/**
+ * Pasta base para dados persistentes.
+ * - Usa DATA_FOLDER se definida
+ * - Caso contrário, cai para /data (padrão de container)
+ */
+const dataFolder = process.env.DATA_FOLDER || '/data'
 
-export async function download(){
-  const res = await fetch(config.addonIcon, {method: 'GET'});
-  if(!res.ok){
-    throw new Error('Network response was not ok');
-  }
-  let extension = null;
-  if(res.headers.has('content-type')){
-    const matches = res.headers.get('content-type').match(/image\/([a-z0-9]+)/i);
-    if(matches && matches.length > 1)extension = matches[1];
-  }
-  if(!extension && res.headers.has('content-disposition')){
-    const matches = res.headers.get('content-disposition').match(/filename\*?=['"]?(?:UTF-\d['"]*)?([^;\r\n"']*)['"]?;?/i);
-    if(matches && matches.length > 1)extension = matches[1].split('.').pop();
-  }
-  if(!extension){
-    throw new Error(`No valid image found: ${res.headers.get('content-type')} / ${res.headers.get('content-disposition')}`);
-  }
-  const location = `${config.dataFolder}/icon.${extension}`;
-  const buffer = await res.arrayBuffer();
-  await writeFile(location, new Uint8Array(buffer));
-  console.log(`Icon downloaded: ${location}`);
-  ICON_LOCATION = location;
-  return location;
-}
-
-export async function getLocation(){
-  return ICON_LOCATION;
-}
+/**
+ * Caminho absoluto do ícone do addon
+ */
+export const iconPath = path.join(
+  dataFolder,
+  'icon.png'
+)
